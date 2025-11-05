3349cba0fb813c55194ef3b4eff951de80b89c8e
Lucas Caceres <lucacaceres@uade.edu.ar>
2025-11-05 00:19:59 -0300

    avance con la ui

--- src/main/app/ui/VentanaPrincipal.java
+++ src/main/app/ui/VentanaPrincipal.java
@@ -6,8 +6,22 @@ import main.modelo.Docente;
 import javax.swing.*;
 
 public class VentanaPrincipal {
-    private JTabbedPane tabbedPane1;
     private JPanel panel1;
+    private JTabbedPane tabPrincipal;
+    private JTabbedPane ventanaPrincipal;
+    private JTextField userMailL;
+    private JPasswordField userPassL;
+    private JButton ingresarButton;
+    private JPasswordField userConfPassR;
+    private JTabbedPane gestion;
+    private JTabbedPane gestionCursos;
+    private JTabbedPane gestionAreas;
+    private JTextField userMailR;
+    private JPasswordField userPassR;
+    private JButton crearCuentaButton;
+    private JLabel tituloR;
+    private JLabel tituloL;
+    private JLabel errorMensaje;
 
     private Alumno alumnoLogueado = null;
     private Docente docenteLogueado = null;
